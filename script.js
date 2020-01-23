a3ba94a99873004fd85791799cf0db77c7752868
christopher12366 <60225120+christopher12366@users.noreply.github.com>
2020-01-23 09:01:15 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+      $(".coins").hide();
+});
+
+$(".closed").click(function() {
+    $("h1").text(" CLICK BOB");
+});
+$(".bob").click(function() {
+    $(".army").show();
+});
+$(".army").hide();
+
+$(".army").click(function() {
+    $(".face").show();
+});
+
+$(".face").hide(function() {
+    
+});
+
+    
